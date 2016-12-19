20b5066f35be4845cf1f843be97bde11240fd9f0
Praveen Kumar Bolisetty <bpkleo2002@gmail.com>
2016-12-20 03:06:27 +0530

    added new files

---
+++ paymentgateway/ccavRequestHandler.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title> Custom Form Kit </title>
+</head>
+<body>
+<center>
+
+<?php include('Crypto.php')?>
+<?php 
+
+	error_reporting(0);
+	
+	$merchant_data='117679';
+	$working_key='52E2F5639BC33F534DF287CC78FB3FD6';//Shared by CCAVENUES
+	$access_code='AVZP68DL23AS77PZSA';//Shared by CCAVENUES
+	
+	foreach ($_POST as $key => $value){
+		$merchant_data.=$key.'='.urlencode($value).'&';
+	}
+
+	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+
+?>
+<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<?php
+echo "<input type=hidden name=encRequest value=$encrypted_data>";
+echo "<input type=hidden name=access_code value=$access_code>";
+?>
+</form>
+</center>
+<script language='javascript'>document.redirect.submit();</script>
+</body>
+</html>
+
