c3e09333a9df8ef1dc92325572cb244de0b5b96b
Praveen Kumar Bolisetty <bpkleo2002@gmail.com>
2016-12-21 12:25:18 +0530

    configured the db for local host

--- db.php
+++ db.php
@@ -1,23 +1,33 @@
 <?php
-$my_path="http://localhost:8061";
-$path="http://jatka.in/";
+
+$my_path = "http://" . $_SERVER['HTTP_HOST'];
+$path = "http://" . $_SERVER['HTTP_HOST'] . "/";
 $DB_host = "localhost";
-$company_url="#";
-$compnay_small_name="Jatka";
-$logo_img_url="logo.png";
-$fav_icon_url="";
-$compnay_name="Jatka.in";
-$DB_user = "nagendra_jin";
-$DB_pass = "nagendra@ipl";
-$DB_name = "jatkaupdate";
-try
-{
-	$db = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$company_url = "#";
+$compnay_small_name = "Jatka";
+$logo_img_url = "logo.png";
+$fav_icon_url = "";
+$compnay_name = "Jatka.in";
+switch ($_SERVER['SERVER_NAME']) {
+    case 'jatka.in' : {
+            $DB_user = "nagendra_jin";
+            $DB_pass = "nagendra@ipl";
+            $DB_name = "jatkaupdate";
+            break;
+        }
+    case "localhost": {
+            $DB_user = "root";
+            $DB_pass = "";
+            $DB_name = "jatkaupdate";
+            break;
+        }
 }
-catch(PDOException $e)
-{
-	echo $e->getMessage();
+
+try {
+    $db = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
 //Set useful variables for paypal form
 //$paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr'; //Test PayPal API URL
@@ -26,7 +36,7 @@ catch(PDOException $e)
 $paypal_url = 'https://www.paypal.com/cgi-bin/webscr'; //Test PayPal API URL
 $paypal_id = 'phanisri.konte@gmail.com'; //Business Email
 
-$return_jsurl = $my_path.'/job-seeker/transactions-payment-success.aspx';
-$return_jsourl = $my_path.'/job-seeker/cw-order/transactions-payment-success.aspx';
-$cancel_jsurl = $my_path.'/job-seeker/transactions-payment-cancelled.aspx';
+$return_jsurl = $my_path . '/job-seeker/transactions-payment-success.aspx';
+$return_jsourl = $my_path . '/job-seeker/cw-order/transactions-payment-success.aspx';
+$cancel_jsurl = $my_path . '/job-seeker/transactions-payment-cancelled.aspx';
 ?>
