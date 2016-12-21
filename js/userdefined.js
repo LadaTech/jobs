ead152a36d2a3f735195d8294a09758371a9492c
Praveen Kumar Bolisetty <bpkleo2002@gmail.com>
2016-12-21 19:28:24 +0530

    added the validations

--- js/userdefined.js
+++ js/userdefined.js
@@ -5,8 +5,8 @@
  */
 
 
-$(function(){
-       $("#identicalForm").validate({
+$(function () {
+    $("#identicalForm").validate({
         rules: {
             Industry: {
                 required: true
@@ -17,10 +17,10 @@ $(function(){
             Experience_level: {
                 required: true
             },
-            exp_yrs:{
+            exp_yrs: {
                 required: true
             },
-            exp_mnths:{
+            exp_mnths: {
                 required: true
             }
         },
@@ -42,6 +42,96 @@ $(function(){
             }
         }
     });
- 
-    
+
+    $("#cwEditPaymentInfo").validate({
+        rules: {
+            iid: {
+                required: true
+            },
+            Domain: {
+                required: true
+            },
+            special_fresher: {
+                required: true
+            },
+            special_1_exp: {
+                required: true
+            },
+            special_2_exp: {
+                required: true
+            },
+            special_3_exp: {
+                required: true
+            },
+            special_4_exp: {
+                required: true
+            },
+            expected_special_delivery: {
+                required: true
+            },
+            general_fresher: {
+                required: true
+            },
+            general_1_exp: {
+                required: true
+            },
+            general_2_exp: {
+                required: true
+            },
+            general_3_exp: {
+                required: true
+            },
+            general_4_exp: {
+                required: true
+            },
+            expected_delivery:{
+                required: true
+            }
+        },
+        messages: {
+            iid: {
+                required: 'Please select industry'
+            },
+            Domain: {
+                required: 'Please selecct domain'
+            },
+            special_fresher: {
+                required: 'Please enter Fresher Price'
+            },
+            special_1_exp: {
+                required: 'Please enter price for 1-3 Experience'
+            },
+            special_2_exp: {
+                required: 'Please enter price for 3-5 Experience'
+            },
+            special_3_exp: {
+                required: 'Please enter price for 5-10 Experience'
+            },
+            special_4_exp: {
+                required: 'Please enter price for 10+ Experience'
+            },
+            expected_special_delivery: {
+                required: 'Please selecct domain'
+            },
+            general_fresher: {
+                required: 'Please enter Fresher Price'
+            },
+            general_1_exp: {
+                required: 'Please enter price for 1-3 Experience'
+            },
+            general_2_exp: {
+                required: 'Please enter price for 3-5 Experience'
+            },
+            general_3_exp: {
+                required: 'Please enter price for 5-10 Experience'
+            },
+            general_4_exp: {
+                required: 'Please enter price for 10+ Experience'
+            },
+            expected_delivery:{
+                required: 'Please enter Expected Delivery'
+            }
+        }
+    });
+
 });
