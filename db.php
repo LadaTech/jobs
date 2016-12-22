d12da59767ae569c5727977f4cfd2a0d848dcf49
raghavender <raghavender.y@evergent.com>
2016-12-22 16:37:42 +0530

    fixed switch case issue and added .htaccess

--- db.php
+++ db.php
@@ -36,7 +36,7 @@ switch ($_SERVER['SERVER_NAME']) {
             $DB_name = "jatkaupdate";
             break;
         }
-
+}
         try {
             $db = new PDO("mysql:host={$DB_host};dbname={$DB_name}", $DB_user, $DB_pass);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
